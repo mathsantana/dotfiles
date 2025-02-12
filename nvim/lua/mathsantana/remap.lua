c47e5e228cf103a364e1531dec4655910fcf279e
Matheus Santana <matheus.santana@ccc.ufcg.edu.br>
2025-02-12 01:02:59 -0300

    feat(remap): more remap fixing

--- nvim/lua/mathsantana/remap.lua
+++ nvim/lua/mathsantana/remap.lua
@@ -30,9 +30,6 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
-
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
@@ -45,7 +42,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>cfg", "<cmd>e ~/.dotfiles/nvim/init.lua<CR>");
+vim.keymap.set("n", "<leader>cfg", "<cmd>e ~/dotfiles/nvim/init.lua<CR>");
 
 vim.keymap.set("n", "<leader>so", function()
 	vim.cmd("so")
